c94a91365f3dba008ba77fd0f26528268084c306
googlebrain@163.com <googlebrain@163.com>
2020-07-01 14:13:33 +0800

    第一次上传

--- springboot01/src/main/java/cn/k/springboot01/controller/HelloWorldController.java
+++ springboot01/src/main/java/cn/k/springboot01/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package cn.k.springboot01.controller;
 
 import cn.k.springboot01.properties.StuComponent;
+import cn.k.springboot01.properties.StuComponent2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,11 +15,15 @@ public class HelloWorldController {
     @Resource
     private StuComponent stuComponent;
 
+    @Resource
+    private StuComponent2 stuComponent2;
+
     @ResponseBody
     @RequestMapping("/helloworld")
     public String helloWorld(String name){
-        System.out.println(name);
-        System.out.println(">>>>>>>>"+stuComponent);
+        System.out.println("请求参数值："+name);
+        System.out.println("获取配置文件值："+stuComponent);
+        System.out.println("获取配置文件值2："+stuComponent2);
         return "当前时间>>>>>"+new Date();
     }
 }
--- springboot01/src/main/java/cn/k/springboot01/properties/StuComponent2.java
+++ springboot01/src/main/java/cn/k/springboot01/properties/StuComponent2.java
@@ -1,14 +1,16 @@
 package cn.k.springboot01.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 /**
  * 获取配置文件数据
  */
 @Component
-@ConfigurationProperties(prefix = "stu")
-public class StuComponent {
+@ConfigurationProperties(prefix = "stu2")
+@PropertySource("classpath:other.properties")
+public class StuComponent2 {
     private String name;
     private String age;
     private String address;
